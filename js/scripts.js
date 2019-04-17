f5b1d6d15a4f68b8561ccf0459628c339d14078e
Zee <zam278@nyu.edu>
2019-04-17 18:58:15 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
-# save seperate json file
+// sets up my mapbox access token so they can track my usage of their basemap services
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmYTQ2YzBmMTM0ZDAyIn0.owNd_Qa7Sw2neNJbK6zc1A';
 
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiemFtMjc4IiwiYSI6ImNqdWQ5YWpneDBzbGQ0ZW5xOGtzcDJ0OGcifQ.PqU5et-zFUgXyhIeGuueqg';
 var map = new mapboxgl.Map({
-
   container: 'mapContainer',
   style: 'mapbox://styles/mapbox/light-v9',
   center: [-74.01, 40.78],
