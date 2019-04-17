06e03f25bc660c50e8c53a01015d9243a3c436e7
Zee <zam278@nyu.edu>
2019-04-17 19:19:25 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,3 @@ var map = new mapboxgl.Map({
   center: [-73.999679, 40.791516],
   zoom: 9.5,
 });
-
-// Add zoom and rotation controls to the map.
-map.addControl(new mapboxgl.NavigationControl());
-
-var popup = new mapboxgl.Popup({ offset: 40 })
-  .setText('Hi Web Mapping 2019 Class!');
-
-var marker = new mapboxgl.Marker()
-  .setLngLat([-73.969145,40.669116])
-  .setPopup(popup)
-  .addTo(map);
-
-studentPizzaShops.forEach(function(studentData) {
-
-  var thisStudentsColor = 'steelblue';
-  if (studentData.nyuprogram === 'wagner') thisStudentsColor = 'orange';
-  if (studentData.nyuprogram === 'cusp') thisStudentsColor = 'purple';
-  if (studentData.nyuprogram === 'adjunct') thisStudentsColor = 'green';
-  if (studentData.nyuprogram === 'global liberal studies') thisStudentsColor = 'yellow';
-
-  new mapboxgl.Marker({
-    color: thisStudentsColor,
-  })
-    .setLngLat([studentData.lng, studentData.lat])
-    .setPopup(new mapboxgl.Popup({ offset: 40 })
-      .setText(`${studentData.name} says their favorite pizza shop is ${studentData.favoritepizzashop}`))
-    .addTo(map);
-})
