d1e2097514ebc10c818da02b5aeb1e10db76607c
Zee <zam278@nyu.edu>
2019-04-17 21:54:25 -0400

    Update StudentData.js

--- js/StudentData.js
+++ js/StudentData.js
@@ -3,61 +3,61 @@ var studentFavoriteSpots = [
 {
   "name": "Meriam",
   "favoritspot": "Balthazar Bakery",
-  "lng": -73.981492,
+  "lag": -73.981492,
   "lat": 40.887379
 },
 {
   "name": "Letal",
   "favoritspot": "Porta Pizza",
-  "lng": -74.0459353,
+  "lag": -74.0459353,
   "lat": 40.720288
 },
 {
   "name": "Saba",
   "favoritspot": "Bucket & Bay",
-  "lng": -74.0342191,
+  "lag": -74.0342191,
   "lat": 40.7374588
 },
 {
   "name": "Salma",
   "favoritspot": "Laffayette Café'",
-  "lng": -73.9958957,
+  "lag": -73.9958957,
   "lat": 40.7275934
 },
 {
   "name": "Roaa",
   "favoritspot": "Blue Strips Cacao Shop",
-  "lng": -73.995091,
+  "lag": -73.995091,
   "lat": 40.7346342
 },
 {
   "name": "May",
   "favoritspot": "Bluestone Lane (Greenwich)",
-  "lng": -74.0030846,
+  "lag": -74.0030846,
   "lat": 40.7358132
 },
 {
   "name": "Aleena",
   "favoritspot": "Edo Sushi",
-  "lng": -73.9934704,
+  "lag": -73.9934704,
   "lat": 40.7377082
 },
 {
   "name": "TJ",
   "favoritspot": "Indeblue",
-  "lng": -75.1642297,
+  "lag": -75.1642297,
   "lat": 39.9485153
 },
 {
   "name": "Fran",
   "favoritspot": "Rittenhouse Square",
-  "lng": -75.1748791,
+  "lag": -75.1748791,
   "lat": 39.9479448
 },
 {
   "name": "Neelima",
   "favoritspot": "Suraya",
-  "lng": -75.1361368,
+  "lag": -75.1361368,
   "lat": 39.9736966
 }
 ]
