04196ee830342e236ff6f0b2c91535cff928b51c
Zee <zam278@nyu.edu>
2019-05-04 22:14:04 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -39,8 +39,8 @@ filmSpots.forEach(function(filmData) {
   })
     .setLngLat([filmData.lon, filmData.lat])
     .setPopup(new mapboxgl.Popup({ offset: 40 })
-      .setHTML('<h3>' + filmData.description +'</h3><p>' + "Year" + "Rating" +
-        filmData.rate + " out of 10" + '</p><p>' +
+      .setHTML('<h3>' + filmData.description +'</h3><p>' + "year" + "Rating" +
+        filmData.rate + " out of 10" + '</p><p>' + "location"
         filmData.location + '</p>' ))
     .addTo(map);
 //add legend to the map
